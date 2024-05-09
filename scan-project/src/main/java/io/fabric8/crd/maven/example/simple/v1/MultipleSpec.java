91a6054343e4b4d4e9fe9a3c4315c7058bf1b73b
Bernhard Strähle <bernhard.straehle@gmail.com>
2024-05-09 21:32:57 +0200

    Remove high-level filtering

--- scan-project/src/main/java/io/fabric8/crd/maven/example/simple/v1/MultipleSpec.java
+++ scan-project/src/main/java/io/fabric8/crd/maven/example/simple/v1/MultipleSpec.java
@@ -1,9 +1,8 @@
 package io.fabric8.crd.maven.example.simple.v1;
 
+import lombok.Data;
+
+@Data
 public class MultipleSpec {
   private String v1;
-
-  public String getV1() {
-    return v1;
-  }
 }
