85fb11f7381132eefb5ca7817794e7981e1715f0
Bernhard Strähle <bernhard.straehle@gmail.com>
2024-12-06 00:33:00 +0100

    Add example for scanning test classes only

---
+++ scan-test-classes-only/src/test/java/io/fabric8/crd/maven/example/CRDGenerationTest.java
@@ -0,0 +1,23 @@
+package io.fabric8.crd.maven.example;
+
+import io.fabric8.kubernetes.api.model.apiextensions.v1.CustomResourceDefinition;
+import io.fabric8.kubernetes.client.utils.KubernetesSerialization;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class CRDGenerationTest {
+
+  @Test
+  void test() throws IOException {
+    KubernetesSerialization serialization = new KubernetesSerialization();
+    try (var in = CRDGenerationTest.class.getResourceAsStream(
+        "/multiples.sample.fabric8.io-v1.yml")) {
+      var crd = serialization.unmarshal(in, CustomResourceDefinition.class);
+      assertNotNull(crd);
+    }
+  }
+
+}
---
+++ scan-test-classes-only/src/test/java/io/fabric8/crd/maven/example/v1/MultipleSpec.java
@@ -0,0 +1,8 @@
+package io.fabric8.crd.maven.example.v1;
+
+import lombok.Data;
+
+@Data
+public class MultipleSpec {
+  private String v1;
+}
